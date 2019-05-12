a340101905ff52df1a7a706b14664e64578be93f
minoru.kamada <minbooo185@gmail.com>
2019-05-12 09:09:27 +0000

    update controller set

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,7 +28,15 @@ class TasksController < ApplicationController
   end
    
   def update
-    
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'task は更新されませんでした'
+      render :edit
+    end
   end
    
   def destroy
