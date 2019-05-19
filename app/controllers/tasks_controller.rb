c4da2188b95418fd4a46f7eb0e4a8cef22a75acd
minoru.kamada <minbooo185@gmail.com>
2019-05-19 11:35:19 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
@@ -13,14 +14,15 @@ class TasksController < ApplicationController
   end
    
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'task が正常に作成されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'task が作成されませんでした'
-      render :new
+      render 'toppages/index'
     end
   end
   
